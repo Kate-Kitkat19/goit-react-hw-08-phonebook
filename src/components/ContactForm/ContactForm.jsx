c5f3914a573673337493175cff13d5074907dde5
Kate-Kitkat19 <katekateee1911@gmail.com>
2023-01-18 01:23:43 +0200

    styled contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,92 +1,105 @@
-import { Formik, ErrorMessage, Field, Form } from 'formik';
+import { Formik, Field } from 'formik';
 import React from 'react';
-
 import { ValidationSchema } from './Validation';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts } from 'redux/contacts/selectors';
 import { addContact } from 'redux/contacts/operations';
-import { Icon } from '@chakra-ui/react';
-import { Alert } from '@chakra-ui/react';
-
 import {
+  Icon,
+  Button,
+  InputLeftAddon,
+  FormControl,
+  FormErrorMessage,
+  Container,
   Input,
   VStack,
   InputGroup,
-  InputLeftElement,
-  Button,
+  useToast
 } from '@chakra-ui/react';
+
 import { PhoneIcon } from '@chakra-ui/icons';
 import { ImUser } from 'react-icons/im';
 
 export const ContactForm = () => {
   const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
-  const INITIAL_VALUES = {
-    name: '',
-    number: '',
-  };
-
-  const handleSubmit = (values, { resetForm }) => {
-    const contactName = values.name.toLowerCase();
-    const isSaved = contacts.find(
-      contact => contact.name.toLowerCase() === contactName
-    );
-    if (isSaved) {
-      alert(`${values.name} is already in contacts`);
-    } else {
-      dispatch(addContact({ name: values.name, number: values.number }));
-    }
-    resetForm();
-  };
+const toast = useToast();
 
   return (
-    <Formik
-      onSubmit={handleSubmit}
-      validationSchema={ValidationSchema}
-      initialValues={{ ...INITIAL_VALUES }}
-    >
-      <Form>
-        <VStack spacing={4}>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<Icon as={ImUser} color="gray.300" />}
-            />
-            <Field
-              as={Input}
-              type="text"
-              name="name"
-              placeholder="Name"
-              width="auto"
-            ></Field>
-            <ErrorMessage as={Alert} status="error" name="name"></ErrorMessage>
-          </InputGroup>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<PhoneIcon color="gray.300" />}
-            />
-            <Field
-              as={Input}
-              type="tel"
-              name="number"
-              placeholder="Phone number"
-              width="auto"
-            ></Field>
-            <ErrorMessage status="error" name="number"></ErrorMessage>
-          </InputGroup>
-
-          <Button
-            type="submit"
-            aria-label="Add contact"
-            size="lg"
-            colorScheme="green"
-          >
-            Submit
-          </Button>
-        </VStack>
-      </Form>
-    </Formik>
+    <Container marginBottom={4} centerContent maxW="sm">
+      <Formik
+        initialValues={{
+          name: '',
+          number: '',
+        }}
+        onSubmit={(values, { resetForm }) => {
+          const contactName = values.name.toLowerCase();
+          const isSaved = contacts.find(
+            contact => contact.name.toLowerCase() === contactName
+          );
+          if (isSaved) {
+            return toast({
+              title: `${values.name} is already in contacts`,
+              position: 'top',
+              status: 'error',
+              duration: 5000,
+              isClosable: true,
+            });
+          } else {
+            dispatch(addContact({ name: values.name, number: values.number }));
+          }
+          resetForm();
+        }}
+        validationSchema={ValidationSchema}
+      >
+        {({ handleSubmit, errors, touched }) => (
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4}>
+              <FormControl isRequired isInvalid={!!errors.name && touched.name}>
+                <InputGroup>
+                  <InputLeftAddon
+                    pointerEvents="none"
+                    children={<Icon as={ImUser} color="gray.300" />}
+                  />
+                  <Field
+                    as={Input}
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    width="full"
+                  ></Field>
+                </InputGroup>
+                <FormErrorMessage>{errors.name}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!errors.number && touched.number}>
+                <InputGroup>
+                  <InputLeftAddon
+                    pointerEvents="none"
+                    children={<PhoneIcon color="gray.300" />}
+                  />
+                  <Field
+                    as={Input}
+                    type="tel"
+                    name="number"
+                    placeholder="Phone number"
+                    width="full"
+                  ></Field>
+                </InputGroup>
+                <FormErrorMessage>{errors.number}</FormErrorMessage>
+              </FormControl>
+              <Button
+                type="submit"
+                aria-label="Add contact"
+                colorScheme="green"
+                width="full"
+              >
+                Add contact
+              </Button>
+            </VStack>
+          </form>
+        )}
+      </Formik>
+    </Container>
   );
 };
 
